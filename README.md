e2e7a630bd37dffc9f9c75207c30c9582c0ea140
tolis-e <tolisemm@yahoo.gr>
2014-01-19 12:48:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# aerogear-js-vertx-sockjs-eventbus-chat
+> Sample example which uses the [AeroGear JS](https://github.com/aerogear/aerogear-js) Vert.x Notifier
+
+## Getting Started
+
+Execute: `./vertxbustest/server.sh`
+
+Open the `aerogear-chat/index.html` in multiple tabs of your web browser and exchange messages.
+
+You can stop the server by executing: `./vertxbustest/server.sh stop`
+
+## Notes
+The client side part is based on the chat example included in the [stomp-websockets](https://github.com/hornetq/hornetq/tree/master/examples/jms/stomp-websockets) HornetQ examples and has been modified to use the AeroGear Vert.x Notifier.
---
+++ aerogear-chat/chat.js
@@ -0,0 +1,86 @@
+$( document ).ready( function () {
+    
+    var client, 
+        destination,
+        onsubscribe;
+
+    $( '#connect_form' ).submit( function ( e ) {
+        
+        e.preventDefault();
+
+        var url = $( '#connect_url' ).val();
+        destination = $( "#destination" ).val();
+
+        // create AeroGear Notifier
+        client = AeroGear.Notifier({
+            name: "vertx",
+            type: "vertx",
+            settings: {
+                autoConnect: true,
+                connectURL: url
+            }
+        }).clients.vertx;
+
+        var ondisconnect = function () {
+            $( '#disconnect' ).fadeOut({
+                duration: 'fast'
+            });
+            $( '#unsubscribe' ).fadeOut({
+                duration: 'fast'
+            });
+            $( '#connect' ).fadeIn();
+            $( '#messages' ).empty();
+            $( '#send_form_input' ).attr( 'disabled', 'disabled' );
+        },
+        onconnect = function () {
+            $( '#connect').fadeOut({
+                duration: 'fast'
+            });
+            $( '#disconnect' ).fadeIn();
+            $( '#unsubscribe' ).fadeIn();
+            $( '#send_form_input' ).removeAttr( 'disabled' );
+
+            onsubscribe = function (message) {
+                $( "#messages" ).append( "<p>" + message + "</p>\n" );
+            };
+
+            // subscribe
+            client.subscribe({
+                address: destination,
+                    callback: onsubscribe
+            }, true);
+        };
+
+        client.connect({
+            onConnect: onconnect,
+            onDisconnect: ondisconnect,
+            onConnectError: function () {
+                console.log( '### Connection error' );
+            }
+        });
+    });
+
+    $( '#disconnect_form' ).submit( function ( e ) {
+        
+        e.preventDefault();
+        client.disconnect();
+    });
+    
+    $( '#unsubscribe_form' ).submit( function ( e ) {
+        e.preventDefault();
+        client.unsubscribe( [{ 
+            address: destination,
+            callback: onsubscribe
+        }]);
+    });
+
+    $( '#send_form' ).submit( function ( e ) {
+
+        e.preventDefault();
+        var text = $( '#send_form_input' ).val();
+        if (text) {
+            client.send( destination, text, true );
+            $('#send_form_input').val( '' );
+        }
+    });
+});
